9d74eb906792c0df78a05f351c1339aef8ce3950
Jono Xia <jono@mozilla.com>
2012-05-04 14:45:14 -0700

    Revamped command links on underworld page; moved menu item to History menu, got rid of toolbar icon.

--- data/ui/underworld.js
+++ data/ui/underworld.js
@@ -1,23 +1,27 @@
 function generateRow(tab) {
   var tr = $("<tr></tr>");
-  tr.append($("<td></td>").html(tab.title));
-  tr.append($("<img></img>").attr("src", tab.favicon));
+  /*tr.append($("<td></td>").html(tab.title));
+  tr.append($("<img></img>").attr("src", tab.favicon));*/
   tr.append($("<td></td>").html(tab.url));
 
   var openLink = $("<a>Reopen</a>").click(function() {
-    window.reviveTab(tab.url);
+    window.removeTab(tab.url, "reopen");
     regenTable();
   });
 
   var killLink = $("<a>Discard</a>").click(function() {
-    window.removeTab(tab.url);
+    window.removeTab(tab.url, "forget");
     regenTable();
   });
 
-  var actionCell = $("<td></td>");
-  actionCell.append(openLink);
-  actionCell.append(killLink);
-  tr.append(actionCell);
+  var archiveLink = $("<a>Archive</a>").click(function() {
+    window.removeTab(tab.url, "archive");
+    regenTable();
+  });
+
+  tr.append( $("<td></td>").append(openLink) );
+  tr.append( $("<td></td>").append(killLink) );
+  tr.append( $("<td></td>").append(archiveLink) );
   return tr;
 }
 
--- data/underworld-content-script.js
+++ data/underworld-content-script.js
@@ -7,10 +7,6 @@ unsafeWindow.onTabUnderworldLoad = function(callback) {
   self.port.emit("getDeadTabs");
 };
 
-unsafeWindow.removeTab = function(url) {
-  self.port.emit("removeTab", url);
-};
-
-unsafeWindow.reviveTab = function(url) {
-  self.port.emit("reviveTab", url);
+unsafeWindow.removeTab = function(url, action) {
+  self.port.emit("removeTab", JSON.stringify({url: url, action: action}));
 };
